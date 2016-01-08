ac586b4eeec7333fd46ee8f0226b3307c594a474
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-01-08 14:33:19 +0200

    have token tests passing

--- test/b9_security_web_token.js
+++ test/b9_security_web_token.js
@@ -6,26 +6,27 @@ describe('b8_security_https_websockets_sanity', function() {
   var happn_client = happn.client;
   var async = require('async');
 
-  var test_secret = 'test_secret';
-  var mode = "embedded";
-  var default_timeout = 4000;
   var happnInstance = null;
-  var test_id;
+  var test_id = Date.now() + '_' + require('shortid').generate();
 
   var http = require('http');
 
-  var http_request_options = {
-    host: '127.0.0.1',
-    port:55000
-  };
+  var adminClient;
+  var testClient;
 
-  function doRequest(path, callback){
+  function doRequest(path, token, callback){
+
+    var http_request_options = {
+      host: '127.0.0.1',
+      port:55000
+    };
+
     http_request_options.path = path;
-    http.request(http_request_options, function(e, response){
+    http_request_options.headers = {'Cookie': 'happn_token=' + token}
 
+    console.log('in doRequest:::', http_request_options);
 
-      
-    }).end();
+    http.request(http_request_options, callback).end();
   }
 
   /*
@@ -38,11 +39,10 @@ describe('b8_security_https_websockets_sanity', function() {
     
     this.timeout(20000);
 
-    test_id = Date.now() + '_' + require('shortid').generate();
 
     try{
       service.create({
-          secureWebRoutes:'*'
+          secure:true
         },function (e, happnInst) {
               if (e)
                 return callback(e);
@@ -58,67 +58,154 @@ describe('b8_security_https_websockets_sanity', function() {
 
   after(function(done) {
 
-    publisherclient.disconnect()
-    .then(listenerclient.disconnect()
+    adminClient.disconnect()
+    .then(testClient.disconnect()
     .then(happnInstance.stop()
     .then(done)))
     .catch(done);
 
-  });
 
-  var publisherclient;
-  var listenerclient;
+  });
 
   /*
     We are initializing 2 clients to test saving data against the database, one client will push data into the 
     database whilst another listens for changes.
   */
-  before('should initialize the clients', function(callback) {
-      this.timeout(default_timeout);
+  before('should initialize the admin client', function(callback) {
+      
+      happn.client.create({
+        config:{username:'_ADMIN', password:'happn'},
+        secure:true
+      })
 
-      try {
-        happn_client.create(function(e, instance) {
+      .then(function(clientInstance){
+        adminClient = clientInstance;
+        callback();
+      })
 
-          if (e) return callback(e);
-
-          publisherclient = instance;
-          happn_client.create(function(e, instance) {
-
-            if (e) return callback(e);
-            listenerclient = instance;
-            callback();
-
-          });
-
-        });
-
-      } catch (e) {
+      .catch(function(e){
         callback(e);
-      }
+      });
 
    });
 
-  it('the server should set up a secure route', function (callback) {
+    var testGroup = {
+      name:'TEST GROUP' + test_id,
+      custom_data:{
+        customString:'custom1',
+        customNumber:0
+      }
+    }
 
-    this.timeout(default_timeout);
+    var testUser = {
+      username:'TEST USER@blah.com' + test_id,
+      password:'TEST PWD',
+      custom_data:{
+        something: 'usefull'
+      }
+    }
+
+    var addedTestGroup;
+    var addedTestuser;
+
+
+    before('creates a group and a user, adds the group to the user, logs in with test user', function(done) {
+
+      happnInstance.services.security.upsertGroup(testGroup, {overwrite:false}, function(e, result){
+
+        if (e) return done(e);
+        addedTestGroup = result;
+
+        happnInstance.services.security.upsertUser(testUser, {overwrite:false}, function(e, result){
+
+          if (e) return done(e);
+          addedTestuser = result;
+
+          happnInstance.services.security.linkGroup(addedTestGroup, addedTestuser, function(e){
+
+            if (e) return done(e);
+
+            happn.client.create({
+              config:{username:testUser.username, password:'TEST PWD'},
+              secure:true
+            })
+
+            .then(function(clientInstance){
+              testClient = clientInstance;
+              done();
+            })
+
+            .catch(function(e){
+              done(e);
+            });
+
+          });
+        });
+      });
+    });
+
+  it('the server should set up a secure route, the admin client should connect ok', function (callback) {
 
     try {
 
       happnInstance.connect.use('/secure/route', function(req, res, next){
 
-        console.log('in secure route:::', JSON.stringify({"secure":"value"}));
-
-        //res.setHeader('Content-Type', 'application/json');
-        res.end();
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"secure":"value"}));
 
       });
 
-      doRequest('/secure/route', function(response){
+      doRequest('/secure/route', adminClient.session.token, function(response){
 
-
-
+        expect(response.statusCode).to.equal(200);
         callback();
 
+      });
+
+    } catch (e) {
+      callback(e);
+    }
+  });
+
+  it('the server should set up another secure route, the test client should fail to connect', function (callback) {
+
+    try {
+
+      happnInstance.connect.use('/secure/route/test', function(req, res, next){
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"secure":"value"}));
+
+      });
+
+      doRequest('/secure/route/test', testClient.session.token, function(response){
+
+        expect(response.statusCode).to.equal(403);
+        callback();
+
+      });
+
+    } catch (e) {
+      callback(e);
+    }
+  });
+
+  it('updates the group associated with the test user to allow gets to the path, the user should succeed in connecting to the url', function (callback) {
+
+    try {
+
+      testGroup.permissions = {'/secure/route/test/GET':{actions:['get']}};
+      
+      happnInstance.services.security.upsertGroup(testGroup, {}, function(e, group){
+        if (e) return done(e);
+        expect(group.permissions['/secure/route/test/GET']).to.eql({actions:['get']});
+        
+         doRequest('/secure/route/test', testClient.session.token, function(response){
+
+          expect(response.statusCode).to.equal(200);
+          callback();
+
+        });
       });
 
     } catch (e) {
