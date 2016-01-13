bba9d70425281e9ab6910804274b63f486278906
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-01-13 09:59:54 +0200

    set up initial test

--- test/c1_security_pubpriv_login.js
+++ test/c1_security_pubpriv_login.js
@@ -40,7 +40,8 @@ describe('c1_security_pubpriv_login', function() {
         happnInstance = happnInst;
 
         service.create({
-            secure:true
+            secure:true,
+            encryptPayloads:true
           },function (e, happnInst) {
           if (e)
             return callback(e);
@@ -68,86 +69,17 @@ describe('c1_security_pubpriv_login', function() {
 
   });
 
-  /*
-    We are initializing 2 clients to test saving data against the database, one client will push data into the 
-    database whilst another listens for changes.
-  */
-  before('should initialize the admin client and create a test user', function(callback) {
-      
-      happn.client.create({
+
+  it('logs in with the test client, without supplying a public key - attempts to encrypt a payload and fails', function (callback) {
+
+    happn.client.create({
         config:{username:'_ADMIN', password:'happn'},
         secure:true
       })
 
       .then(function(clientInstance){
         adminClient = clientInstance;
-        callback();
-      })
-
-      .catch(function(e){
-        callback(e);
-      });
-
-   });
-
-    var testGroup = {
-      name:'TEST GROUP' + test_id,
-      custom_data:{
-        customString:'custom1',
-        customNumber:0
-      }
-    }
-
-    var testUser = {
-      username:'TEST USER@blah.com' + test_id,
-      password:'TEST PWD',
-      custom_data:{
-        something: 'usefull'
-      }
-    }
-
-    var addedTestGroup;
-    var addedTestuser;
-
-    var keyPair = testServices.security.generateKeyPair();
-
-    before('creates a group and a user, adds the group to the user, logs in with test user', function(done) {
-
-      happnInstance.services.security.upsertGroup(testGroup, {overwrite:false}, function(e, result){
-
-        if (e) return done(e);
-        addedTestGroup = result;
-
-        happnInstance.services.security.upsertUser(testUser, {overwrite:false}, function(e, result){
-
-          if (e) return done(e);
-          addedTestuser = result;
-
-          happnInstance.services.security.linkGroup(addedTestGroup, addedTestuser, function(e){
-
-            if (e) return done(e);
-
-            
-
-          });
-        });
-      });
-    });
-
-  it('logs in with the test client, without supplying a public key - attempts to encrypt a payload and fails', function (callback) {
-
-    happn.client.create({
-        config:{
-          username:testUser.username, 
-          password:'TEST PWD',
-        },
-        secure:true
-      })
-
-      .then(function(clientInstance){
-        testClient = clientInstance;
-        
-        testClient.set('/an/encrypted/payload/target', {"encrypted":"test"}, {encryptPayload:true}, function(e, response){
+        adminClient.set('/an/encrypted/payload/target', {"encrypted":"test"}, {encryptPayload:true}, function(e, response){
 
           expect(e.toString()).to.equal('Error:missing session secret for encrypted payload, did you set the publicKey config option when creating the client?');
           callback();
@@ -158,36 +90,36 @@ describe('c1_security_pubpriv_login', function() {
       .catch(function(e){
         callback(e);
       });
+    });
 
   });
 
   it('logs in with the test client, supplying a public key - receives a sessionSecret and encrypts a payload using the option', function (callback) {
 
     happn.client.create({
-      config:{
-        username:testUser.username, 
-        password:'TEST PWD',
-        publicKey:keyPair.publicKey.toString();
-      },
-      secure:true
-    })
+        config:{username:'_ADMIN', password:'happn'},
+        secure:true,
+        publicKey:keyPair.publicKey.toString()
+      })
 
-    .then(function(clientInstance){
-      testClient = clientInstance;
-      expect(clientInstance.session.secret).to.not.equal(null);
-      
-      testClient.set('/an/encrypted/payload/target', {"encrypted":"test"}, {encryptPayload:true}, function(e, response){
+      .then(function(clientInstance){
 
-        expect(e).to.equal(null);
-        callback();
+        adminClient = clientInstance;
 
+         adminClient.set('/an/encrypted/payload/target', {"encrypted":"test"}, {encryptPayload:true}, function(e, response){
+
+          expect(e).to.equal(null);
+          callback();
+
+        });
+
+      })
+
+      .catch(function(e){
+        callback(e);
       });
-
-    })
-
-    .catch(function(e){
-      callback(e);
     });
+
 
   });
 
