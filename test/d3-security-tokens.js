df404ba9660f0ede1951b7a69046f2abc5032626
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-08-21 19:36:32 +0200

    updated login to allow for nonce retrieval

--- test/d3-security-tokens.js
+++ test/d3-security-tokens.js
@@ -1,17 +1,15 @@
 describe('d3-security-tokens', function () {
 
-  require('benchmarket').start();
-  after(require('benchmarket').store());
+  // TODO:benchmarket stuff
+  // require('benchmarket').start();
+  // after(require('benchmarket').store());
 
   var expect = require('expect.js');
   var happn = require('../lib/index');
   var service = happn.service;
-  var happn_client = happn.client;
-
-  var test_secret = 'test_secret';
-  var happnInstance = null;
-
+  var async = require('async');
   var uuid = require('node-uuid');
+  var Logger = require('happn-logger');
 
   /*
    This test demonstrates starting up the happn service -
@@ -19,9 +17,9 @@ describe('d3-security-tokens', function () {
    the logon session. The utils setting will set the system to log non priority information
    */
 
-  after(function (done) {
-    happnInstance.stop(done);
-  });
+  // after(function (done) {
+  //   happnInstance.stop(done);
+  // });
 
   var serviceConfig = {
     services:{
@@ -115,7 +113,163 @@ describe('d3-security-tokens', function () {
       instance.stop({reconnect:false},callback);
   };
 
-  it('should test the loginDigest function of the happn client', function(){
+  var mockServices = function(callback){
+
+    var testConfigs = {};
+
+    testConfigs.data = {};
+
+    testConfigs.crypto = {};
+
+    var testServices = {};
+
+    testServices.data = require('../lib/services/data_embedded/service');
+    testServices.crypto = require('../lib/services/crypto/service');
+
+    var happnMock = {services: {}};
+
+    happnMock.utils = require('../lib/utils');
+
+    async.eachSeries(['data', 'crypto'], function (serviceName, eachServiceCB) {
+
+      testServices[serviceName] = new testServices[serviceName]({logger: Logger});
+      testServices[serviceName].happn = happnMock;
+
+      testServices[serviceName].initialize(testConfigs[serviceName], function (e, instance) {
+        if (e)  return eachServiceCB(e);
+
+        happnMock.services[serviceName] = testServices[serviceName];
+
+        eachServiceCB();
+
+      });
+    }, function(e){
+
+      if (e) return callback(e);
+      callback(null, happnMock);
+
+    });
+  };
+
+  it('should test the sign and fail to verify function of the crypto service, bad digest', function(done){
+
+    mockServices(function(e, happnMock){
+
+      if (e) return done(e);
+
+      var Crypto = require('happn-util-crypto');
+      var crypto = new Crypto();
+
+      var testKeyPair = crypto.createKeyPair();
+
+      var nonce = crypto.createHashFromString(uuid.v4().toString());
+      var digest = crypto.sign(nonce, testKeyPair.privateKey);
+
+      var verifyResult = happnMock.services.crypto.verify(nonce, 'a dodgy digest', testKeyPair.publicKey);
+
+      expect(verifyResult).to.be(false);
+
+      done();
+
+    });
+
+  });
+
+  it('should test the sign and fail to verify function of the crypto service, bad nonce', function(done){
+
+    mockServices(function(e, happnMock){
+
+      if (e) return done(e);
+
+      var Crypto = require('happn-util-crypto');
+      var crypto = new Crypto();
+
+      var testKeyPair = crypto.createKeyPair();
+
+      var nonce = crypto.createHashFromString(uuid.v4().toString());
+      var digest = crypto.sign(nonce, testKeyPair.privateKey);
+
+      var verifyResult = happnMock.services.crypto.verify('a dodgy nonce', digest, testKeyPair.publicKey);
+
+      expect(verifyResult).to.be(false);
+
+      done();
+
+    });
+
+  });
+
+  it('should test the sign and verify function of the crypto service', function(done){
+
+    mockServices(function(e, happnMock){
+
+      if (e) return done(e);
+
+      var Crypto = require('happn-util-crypto');
+      var crypto = new Crypto();
+
+      var testKeyPair = crypto.createKeyPair();
+
+      var nonce = crypto.createHashFromString(uuid.v4().toString());
+      var digest = crypto.sign(nonce, testKeyPair.privateKey);
+
+      var verifyResult = happnMock.services.crypto.verify(nonce, digest, testKeyPair.publicKey);
+
+      expect(verifyResult).to.be(true);
+
+      done();
+
+    });
+
+  });
+
+  it('should test the sign and verify function of the crypto service, from a generated nonce', function(done){
+
+    mockServices(function(e, happnMock){
+
+      if (e) return done(e);
+
+      var Crypto = require('happn-util-crypto');
+      var crypto = new Crypto();
+
+      var testKeyPair = crypto.createKeyPair();
+
+      var nonce = happnMock.services.crypto.generateNonce();
+      var digest = crypto.sign(nonce, testKeyPair.privateKey);
+
+      var verifyResult = happnMock.services.crypto.verify(nonce, digest, testKeyPair.publicKey);
+
+      expect(verifyResult).to.be(true);
+
+      done();
+
+    });
+
+  });
+
+  it('should test the __prepareLogin method', function(done){
+
+    var happn = require('../lib/index')
+    var happn_client = happn.client;
+
+    var clientInstance = happn_client.__instance({
+      username:'_ADMIN',
+      keyPair:{
+        publicKey: 'AlHCtJlFthb359xOxR5kiBLJpfoC2ZLPLWYHN3+hdzf2',
+        privateKey:'Kd9FQzddR7G6S9nJ/BK8vLF83AzOphW2lqDOQ/LjU4M='
+      }
+    });
+
+    clientInstance.performRequest = function(path, action, data, options, callback){
+
+    };
+
+    //loginParameters, callback
+    clientInstance.__prepareLogin();
+
+  });
+
+  xit('should test the login function of the happn client, passing in a digest', function(){
 
     var happn = require('../lib/index')
     var happn_client = happn.client;
@@ -128,13 +282,20 @@ describe('d3-security-tokens', function () {
         }
     });
 
-    clientInstance.performRequest = function(path, action, data, parameters, callback){
+    clientInstance.performRequest = function(path, action, data, options, callback){
 
       var nonce_requests = {};
 
+      if (!options) options = {};
+
+      mockServices(function(e, happnMock){
+
       if (action == 'request-nonce'){
 
-        var nonce = uuid.v4();
+        var Crypto = require('happn-util-crypto');
+        var crypto = new Crypto();
+
+        var nonce = crypto.generateNonce();
 
         var request = {
           nonce:nonce,
@@ -149,16 +310,28 @@ describe('d3-security-tokens', function () {
 
         }.bind(request), 3000);
 
+        callback(null, nonce);
       }
 
       if (action == 'login'){
 
+        if (data.digest){
+
+          if (nonce_requests[data.nonce] !== null){
+
+            clearTimeout(nonce_requests[data.nonce].__timedOut);
+
+            happnMock.services.crypto.verify(data.nonce, data.digest, data.publicKey);
+
+          } else callback(new Error('could not find nonce request for nonce: ' + data.nonce));
+        }
       }
-    };
 
-    clientInstance.loginDigest()
+      clientInstance.login();
 
-  });
+    });
+
+  };
 
   xit('should create a user with a public key, then login using a signature', function (callback) {
 
@@ -167,17 +340,18 @@ describe('d3-security-tokens', function () {
     var config =  {
     sessionTokenSecret:"absolutely necessary if you want tokens to carry on working after a restart",
     profiles:[ //profiles are in an array, in descending order of priority, so if you fit more than one profile, the top profile is chosen
-      {
-        name:"web-session",
-        map:{
-          user:['WEB_SESSION'],
-          session_type:1//token stateless
-        },
-        policy:{
-          session_ttl: Infinity,
-          session_inactivity_threshold:2000//this is costly, as we need to store state on the server side
+        {
+          name:"web-session",
+          map:{
+            user:['WEB_SESSION'],
+            session_type:1//token stateless
+          },
+          policy:{
+            session_ttl: Infinity,
+            session_inactivity_threshold:2000//this is costly, as we need to store state on the server side
+          }
         }
-      }
+      ]
     };
 
     getService(config, function (e, instance) {
