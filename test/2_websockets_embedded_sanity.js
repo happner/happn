c6865ff5dce11fe0a0c400fbb1b9ed4d08618e7c
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-09-29 18:11:14 +0200

    message being passed to system

--- test/2_websockets_embedded_sanity.js
+++ test/2_websockets_embedded_sanity.js
@@ -28,29 +28,7 @@ describe('2_websockets_embedded_sanity', function () {
     test_id = Date.now() + '_' + require('shortid').generate();
 
     try {
-      service.create({
-        mode: 'embedded',
-        services: {
-          auth: {
-            path: './services/auth/service.js',
-            config: {
-              authTokenSecret: 'a256a2fd43bf441483c5177fc85fd9d3',
-              systemSecret: test_secret
-            }
-          },
-          data: {
-            path: './services/data_embedded/service.js',
-            config: {}
-          },
-          pubsub: {
-            path: './services/pubsub/service.js'
-          }
-        },
-        utils: {
-          log_level: 'info|error|warning',
-          log_component: 'prepare'
-        }
-      }, function (e, happnInst) {
+      service.create(function (e, happnInst) {
         if (e)
           return callback(e);
 
