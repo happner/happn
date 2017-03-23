e5a6675b1804e51ed91755d865a4e864a54e8720
JohanvdWest <jvdwest@gmail.com>
2017-03-23 10:19:12 +0200

    issue-160 - Destroy client if initialize failed in create.

--- test/e7-login-failures-reconnect.js
+++ test/e7-login-failures-reconnect.js
@@ -328,7 +328,7 @@ describe(require('path').basename(__filename), function () {
       })
   });
 
-  it.only('kills a client that is started with create and fails to login', function (done) {
+  it('kills a client that is started with create and fails to login', function (done) {
     Happn.client.create(
       {
         config: {
